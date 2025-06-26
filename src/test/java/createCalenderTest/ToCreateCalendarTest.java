c30a5dece7d3de689a1546c31f025c8f59d2017c
Admin <Admin@DESKTOP-GUADMG8>
2025-06-26 15:17:09 +0530

    Written contact and org test

--- src/test/java/createCalenderTest/ToCreateCalendarTest.java
+++ src/test/java/createCalenderTest/ToCreateCalendarTest.java
@@ -5,6 +5,7 @@ public class ToCreateCalendarTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println("Hi");
+       System.out.println("Contact created");
 	}
 
 }
